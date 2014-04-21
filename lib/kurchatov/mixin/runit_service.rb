92b39367f324a7f08350d1ccfe377d936307693d
Vasiliev Dmitry <vadv.mkn@gmail.com>
2014-04-21 15:45:10 +0400

    new helpers

--- lib/kurchatov/mixin/runit_service.rb
+++ lib/kurchatov/mixin/runit_service.rb
@@ -4,7 +4,11 @@ module Kurchatov
 
       def runit_service_stat(service)
         return "unknown" unless File.exitst?("/etc/sv/#{service}/supervise/stat")
-        File.read("/etc/sv/#{service}/supervise/stat")
+        File.read("/etc/sv/#{service}/supervise/stat").chomp
+      end
+
+      def runit_service_running?(service)
+        runit_service_stat(service) == "run"
       end
 
     end
--- lib/kurchatov/version.rb
+++ lib/kurchatov/version.rb
@@ -1,3 +1,3 @@
 module Kurchatov
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
